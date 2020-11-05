825b388a8e9df2d0fa95cff18c818401a61c800b
zaki <ahmedzakariya355@gmail.com>
2020-11-05 15:32:35 -0500

    why this won't work

--- src/.new/wow.jl
+++ src/.new/wow.jl
@@ -102,28 +102,52 @@ function transprob( grid::Array{Array{Float64,1},1}, pos::Tuple{Int64, Int64}, d
 		right = move(pos, rightDir)
 		# println(left, right, straight, behind)
 
+		if(getbehind)
+			# if( notblocked(grid, behind) ) # Prob of square behind current ot move to current
+			# 	push!(parent_pos, (behind,Drift[STRAIGHT]*grid[behind[1]][behind[2]], behind,Drift[STRAIGHT]))
+			# end
+			if( !notblocked(grid, straight)) # Bounce
+				push!(parent_pos, (pos,Drift[STRAIGHT]*grid[pos[1]][pos[2]], pos,Drift[STRAIGHT] ))
+			# elseif( notblocked(grid, straight) && getbehind)
+			# 	push!(parent_pos, (straight,Drift[STRAIGHT]*grid[straight[1]][straight[2]], straight,Drift[STRAIGHT] ))
 
-		if( notblocked(grid, behind) ) # Prob of square behind current ot move to current
-			push!(parent_pos, (behind,Drift[STRAIGHT]*grid[behind[1]][behind[2]], behind,Drift[STRAIGHT]))
-		end
-		if( !notblocked(grid, straight)) # Bounce
-			push!(parent_pos, (pos,Drift[STRAIGHT]*grid[pos[1]][pos[2]], pos,Drift[STRAIGHT] ))
-		elseif( notblocked(grid, straight) && getbehind)
-			push!(parent_pos, (straight,Drift[STRAIGHT]*grid[straight[1]][straight[2]], straight,Drift[STRAIGHT] ))
+			end
 
-		end
+			if(notblocked(grid, left))
+				push!(parent_pos, (left,Drift[LEFT]*grid[left[1]][left[2]], pos,Drift[LEFT] ))
+			else
+				push!(parent_pos, (pos,Drift[LEFT]*grid[pos[1]][pos[2]], pos,Drift[LEFT] ))
+			end
 
-		if(notblocked(grid, left))
-			push!(parent_pos, (left,Drift[LEFT]*grid[left[1]][left[2]], pos,Drift[LEFT] ))
+			if(notblocked(grid, right))
+				push!(parent_pos, (right,Drift[RIGHT]*grid[right[1]][right[2]], pos,Drift[RIGHT]))
+			else
+				push!(parent_pos, (pos,Drift[RIGHT]*grid[pos[1]][pos[2]], pos,Drift[RIGHT] ))
+			end
 		else
-			push!(parent_pos, (pos,Drift[LEFT]*grid[pos[1]][pos[2]], pos,Drift[LEFT] ))
+			if( notblocked(grid, behind) ) # Prob of square behind current ot move to current
+				push!(parent_pos, (behind,Drift[STRAIGHT]*grid[behind[1]][behind[2]], behind,Drift[STRAIGHT]))
+			end
+			if( !notblocked(grid, straight)) # Bounce
+				push!(parent_pos, (pos,Drift[STRAIGHT]*grid[pos[1]][pos[2]], pos,Drift[STRAIGHT] ))
+			# elseif( notblocked(grid, straight) && getbehind)
+			# 	push!(parent_pos, (straight,Drift[STRAIGHT]*grid[straight[1]][straight[2]], straight,Drift[STRAIGHT] ))
+
+			end
+
+			if(notblocked(grid, left))
+				push!(parent_pos, (left,Drift[LEFT]*grid[left[1]][left[2]], pos,Drift[LEFT] ))
+			else
+				push!(parent_pos, (pos,Drift[LEFT]*grid[pos[1]][pos[2]], pos,Drift[LEFT] ))
+			end
+
+			if(notblocked(grid, right))
+				push!(parent_pos, (right,Drift[RIGHT]*grid[right[1]][right[2]], pos,Drift[RIGHT]))
+			else
+				push!(parent_pos, (pos,Drift[RIGHT]*grid[pos[1]][pos[2]], pos,Drift[RIGHT] ))
+			end
 		end
 
-		if(notblocked(grid, right))
-			push!(parent_pos, (right,Drift[RIGHT]*grid[right[1]][right[2]], pos,Drift[RIGHT]))
-		else
-			push!(parent_pos, (pos,Drift[RIGHT]*grid[pos[1]][pos[2]], pos,Drift[RIGHT] ))
-		end
 	end
 	if(dir==WEST)
 		arr=_gen_parts(WEST, SOUTH, NORTH, EAST, grid, pos)
@@ -156,18 +180,19 @@ function predict(grid::Array{Array{Float64,1}}, dir::Direction)
 end
 
 function smoothpart( grid::Array{Array{Float64,1}}, last_grid::Array{Array{Float64,1}}, Bgrid::Array{Array{Float64,1}},  evidence::Tuple{SquareType, SquareType, SquareType, SquareType},  dir::Direction, pos::Tuple{Int64, Int64})
-	parent_pos=transprob(grid, pos, dir, false )
+	parent_pos=transprob(grid, pos, dir, true )
 	x=0
 	for i in parent_pos
 		tmp_pos = i[1]
 		prob = i[2]
 		drift = i[4]
 		y=cep(grid, tmp_pos, evidence)* Bgrid[tmp_pos[1]][tmp_pos[2]]* i[4]
-		if(pos == (1,1))
-			println("VALUE AT", pos, "==\t", y, ": because \t", tmp_pos, "\t", cep(grid, tmp_pos, evidence),"*	", prob, "*\t", Bgrid[tmp_pos[1]][tmp_pos[2]],"*	", drift, "\t", last_grid[pos[1]][pos[2] ])
-		end
+		# if(pos == (1,4))
+		println("VALUE AT", pos, "==\t", round(y,digits=4), ": because    \t\t\t", tmp_pos, "\t", cep(grid, tmp_pos, evidence),"*	", prob, "*\t", Bgrid[tmp_pos[1]][tmp_pos[2]],"*	", drift, "\t", last_grid[pos[1]][pos[2] ])
+		# end
 		x+=y
 	end
+	println()
 	(x, x *last_grid[pos[1]][pos[2]])
 end
 
@@ -188,8 +213,8 @@ function smooth( grid::Array{Array{Float64,1}}, last_grid::Array{Array{Float64,1
 		end
 	end
 	# print_grid(SP); println(); print_grid(B); println(); println("SUM: ", sum(sum(SP)))
+	 println("SUM: ", sum(sum(SP)))
 	SP/=sum(sum(SP))
-	# print_grid(SP); println(); print_grid(B); println()
 	(SP, B)
 end
 tmp=filter(grid, (OPEN, OPEN, OPEN, OPEN))
@@ -225,10 +250,18 @@ Bgrid = [
 # println("\nPrediction after Action N")
 # print_grid(tmp4)
 # println("\nFiltering after Evidence [1, 1, 0, 1]")
-# print_grid(tmp5)
+print_grid(tmp5); println()
 # println("\nSmoothing last location [1, 1, 0, 1]")
+# (a,b)=smooth(tmp5,tmp3,Bgrid,(CLOSED,CLOSED,OPEN,CLOSED),NORTH)
 (a,b)=smooth(tmp5,tmp3,Bgrid,(CLOSED,CLOSED,OPEN,CLOSED),NORTH)
-# print_grid(a)
+for row in Iterators.reverse(1:6)
+	for col in Iterators.reverse(1:5)
+		if(notblocked(tmp3, (row,col)))
+			smoothpart(tmp5,tmp3,Bgrid,(CLOSED,CLOSED,OPEN,CLOSED),NORTH, (row, col))
+		end
+	end
+end
+print_grid(a)
 # (a2,c)=smooth(tmp5,tmp,b,(CLOSED,CLOSED,OPEN,CLOSED),NORTH)
 # println("\nSmoothing last 2nd location [1, 1, 0, 1]")
 # print_grid(a2)
